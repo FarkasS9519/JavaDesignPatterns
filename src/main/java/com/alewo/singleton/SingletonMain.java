1060162bea7f4e148aa95bbf07a0bddb09041bcb
Alexander Wolf <farkas.sanyi180@gmail.com>
2019-03-19 14:59:42 +0100

    [Initial commit]   - Created the project   - Added gradle

---
+++ src/main/java/com/alewo/singleton/SingletonMain.java
@@ -0,0 +1,9 @@
+package com.alewo.singleton;
+
+public class SingletonMain {
+
+
+    public static void main(String[] args) {
+        System.out.println("Java Design Patterns - Singleton");
+    }
+}
